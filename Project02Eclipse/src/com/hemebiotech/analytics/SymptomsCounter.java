fde3b6850aa71ffc8704d1f9e64cf3ae4fd3b643
Gauthier Cane <gauthier.cane@gmail.com>
2020-01-22 15:53:19 +0100

    New Class with Main

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomsCounter.java
@@ -0,0 +1,10 @@
+package com.hemebiotech.analytics;
+
+public class SymptomsCounter {
+
+	public static void main(String []args) throws Exception {
+
+        AnalyticsCounter Counter = new AnalyticsCounter();
+        Counter.Counter();
+	}
+}
